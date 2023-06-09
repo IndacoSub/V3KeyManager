5d4abd7eb7ff33acff9899ca230b727fd1b09129
IndacoSub <67112008+IndacoSub@users.noreply.github.com>
2023-06-09 12:52:19 +0200

    Add new buttons

--- V3KeyManager/V3KeyManager.Designer.cs
+++ V3KeyManager/V3KeyManager.Designer.cs
@@ -30,13 +30,21 @@
 		{
 			OpenConfigButton = new Button();
 			SaveConfigButton = new Button();
+			EditVersionButton = new Button();
+			EditWindowSettingsButton = new Button();
+			EditPadSettingsButton = new Button();
+			EditMouseSettingsButton = new Button();
+			EditEffectSettingsButton = new Button();
+			EditKeyboardSettingsButton = new Button();
+			ResetChangesButton = new Button();
+			LaunchGameButton = new Button();
 			SuspendLayout();
 			// 
 			// OpenConfigButton
 			// 
-			OpenConfigButton.Location = new Point(23, 370);
+			OpenConfigButton.Location = new Point(13, 383);
 			OpenConfigButton.Name = "OpenConfigButton";
-			OpenConfigButton.Size = new Size(132, 58);
+			OpenConfigButton.Size = new Size(120, 58);
 			OpenConfigButton.TabIndex = 0;
 			OpenConfigButton.Text = "Open Config";
 			OpenConfigButton.UseVisualStyleBackColor = true;
@@ -44,19 +52,99 @@
 			// 
 			// SaveConfigButton
 			// 
-			SaveConfigButton.Location = new Point(181, 372);
+			SaveConfigButton.Location = new Point(139, 383);
 			SaveConfigButton.Name = "SaveConfigButton";
-			SaveConfigButton.Size = new Size(145, 56);
+			SaveConfigButton.Size = new Size(120, 58);
 			SaveConfigButton.TabIndex = 1;
 			SaveConfigButton.Text = "Save Config As...";
 			SaveConfigButton.UseVisualStyleBackColor = true;
 			SaveConfigButton.Click += SaveConfigButton_Click;
 			// 
+			// EditVersionButton
+			// 
+			EditVersionButton.Location = new Point(13, 317);
+			EditVersionButton.Name = "EditVersionButton";
+			EditVersionButton.Size = new Size(120, 60);
+			EditVersionButton.TabIndex = 2;
+			EditVersionButton.Text = "Edit Version";
+			EditVersionButton.UseVisualStyleBackColor = true;
+			// 
+			// EditWindowSettingsButton
+			// 
+			EditWindowSettingsButton.Location = new Point(139, 317);
+			EditWindowSettingsButton.Name = "EditWindowSettingsButton";
+			EditWindowSettingsButton.Size = new Size(120, 60);
+			EditWindowSettingsButton.TabIndex = 3;
+			EditWindowSettingsButton.Text = "Edit Window Settings";
+			EditWindowSettingsButton.UseVisualStyleBackColor = true;
+			// 
+			// EditPadSettingsButton
+			// 
+			EditPadSettingsButton.Location = new Point(265, 317);
+			EditPadSettingsButton.Name = "EditPadSettingsButton";
+			EditPadSettingsButton.Size = new Size(120, 60);
+			EditPadSettingsButton.TabIndex = 4;
+			EditPadSettingsButton.Text = "Edit Pad Settings";
+			EditPadSettingsButton.UseVisualStyleBackColor = true;
+			// 
+			// EditMouseSettingsButton
+			// 
+			EditMouseSettingsButton.Location = new Point(391, 317);
+			EditMouseSettingsButton.Name = "EditMouseSettingsButton";
+			EditMouseSettingsButton.Size = new Size(120, 60);
+			EditMouseSettingsButton.TabIndex = 5;
+			EditMouseSettingsButton.Text = "Edit Mouse Settings";
+			EditMouseSettingsButton.UseVisualStyleBackColor = true;
+			// 
+			// EditEffectSettingsButton
+			// 
+			EditEffectSettingsButton.Location = new Point(517, 317);
+			EditEffectSettingsButton.Name = "EditEffectSettingsButton";
+			EditEffectSettingsButton.Size = new Size(120, 60);
+			EditEffectSettingsButton.TabIndex = 6;
+			EditEffectSettingsButton.Text = "Edit Effect Settings";
+			EditEffectSettingsButton.UseVisualStyleBackColor = true;
+			// 
+			// EditKeyboardSettingsButton
+			// 
+			EditKeyboardSettingsButton.Location = new Point(643, 317);
+			EditKeyboardSettingsButton.Name = "EditKeyboardSettingsButton";
+			EditKeyboardSettingsButton.Size = new Size(120, 60);
+			EditKeyboardSettingsButton.TabIndex = 7;
+			EditKeyboardSettingsButton.Text = "Edit Keyboard Settings";
+			EditKeyboardSettingsButton.UseVisualStyleBackColor = true;
+			// 
+			// ResetChangesButton
+			// 
+			ResetChangesButton.Location = new Point(265, 383);
+			ResetChangesButton.Name = "ResetChangesButton";
+			ResetChangesButton.Size = new Size(120, 58);
+			ResetChangesButton.TabIndex = 8;
+			ResetChangesButton.Text = "Reset Changes";
+			ResetChangesButton.UseVisualStyleBackColor = true;
+			// 
+			// LaunchGameButton
+			// 
+			LaunchGameButton.Location = new Point(391, 383);
+			LaunchGameButton.Name = "LaunchGameButton";
+			LaunchGameButton.Size = new Size(120, 58);
+			LaunchGameButton.TabIndex = 9;
+			LaunchGameButton.Text = "Launch Game";
+			LaunchGameButton.UseVisualStyleBackColor = true;
+			// 
 			// V3KeyManager
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = new Size(775, 450);
+			Controls.Add(LaunchGameButton);
+			Controls.Add(ResetChangesButton);
+			Controls.Add(EditKeyboardSettingsButton);
+			Controls.Add(EditEffectSettingsButton);
+			Controls.Add(EditMouseSettingsButton);
+			Controls.Add(EditPadSettingsButton);
+			Controls.Add(EditWindowSettingsButton);
+			Controls.Add(EditVersionButton);
 			Controls.Add(SaveConfigButton);
 			Controls.Add(OpenConfigButton);
 			Name = "V3KeyManager";
@@ -68,5 +156,13 @@
 
 		private Button OpenConfigButton;
 		private Button SaveConfigButton;
+		private Button EditVersionButton;
+		private Button EditWindowSettingsButton;
+		private Button EditPadSettingsButton;
+		private Button EditMouseSettingsButton;
+		private Button EditEffectSettingsButton;
+		private Button EditKeyboardSettingsButton;
+		private Button ResetChangesButton;
+		private Button LaunchGameButton;
 	}
 }
